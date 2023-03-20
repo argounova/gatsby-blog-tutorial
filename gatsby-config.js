c47a2f7c5a29e02e9102150fa30f8014b1aa9c7b
Volantis <putzstuck@gmail.com>
2023-03-20 13:01:17 -0400

    tutorial part 4 completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog Tutorial`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Gatsby Blog Tutorial",
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Nissan Z Proto in white with yellow chrome te37 advan wheels"
+        src="https://flying-lugnut-product-card-images.s3.amazonaws.com/nissan-z-proto-white-yellow-01.jpg"
+      />
     </Layout>
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title='Home Page' />
 
 export default IndexPage
